8e43fc714ae733c56d054fa98a5d36cd0006eded
German <codinggerman1@gmail.com>
2023-02-24 14:08:00 +0100

    раскомментировал

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 //1. Функция длины строки
 
-// const isLessOrEqual = (string, length) => string.length <= length;
+const isLessOrEqual = (string, length) => string.length <= length;
 
 // isLessOrEqual('привет', 7);
 
@@ -9,7 +9,7 @@
 // функция может принимать строку в разном регистре
 // функция может содержать пробелы и пробелы не должны влиять на палиндром
 
-/* const isPalindrom = (string) => {
+const isPalindrom = (string) => {
   const tempString = string
     .toLowerCase()
     .replaceAll(' ','');
@@ -20,36 +20,36 @@
   }
 return tempString === reverseString
 }
-isPalindrom('Лёша на полке клопа нашёл') */
+isPalindrom('Лёша на полке клопа нашёл')
 
 
 // 3. функция возвращающая целое число
-// const extractNumber = (string) => {
-//     if (typeof string === 'number') {
-//       return string;
-//     }
-//     let result ='';
-//     for(let i = 0; i < string.length; i++){
-//       if (!Number.isNaN(parseInt(string.at(i),10)));
-//     result += string.at(i)
-//   }
-//   return parseInt(result,10);
-// }
+const extractNumber = (string) => {
+    if (typeof string === 'number') {
+      return string;
+    }
+    let result ='';
+    for(let i = 0; i < string.length; i++){
+      if (!Number.isNaN(parseInt(string.at(i),10)));
+    result += string.at(i)
+  }
+  return parseInt(result,10);
+}
 
 
 // 4. формирование адресов
-// eslint-disable-next-line no-unused-vars
-// const padStart = (string, minLength, pad) => {
-//   const actualPad = minLength - string.length;
-//   if (actualPad <= 0) {
-//     return string;
-//   }
-//   const tempPad = pad.slice(0, actualPad % pad.length);
+eslint-disable-next-line no-unused-vars
+const padStart = (string, minLength, pad) => {
+  const actualPad = minLength - string.length;
+  if (actualPad <= 0) {
+    return string;
+  }
+  const tempPad = pad.slice(0, actualPad % pad.length);
 
-//   const tempRepeat = pad.repeat(actualPad / pad.length);
+  const tempRepeat = pad.repeat(actualPad / pad.length);
 
-//   return tempPad + tempRepeat + string;
-// };
+  return tempPad + tempRepeat + string;
+};
 
 // вариант решения с while
 // const padStart = (string, minLength, pad) => {
