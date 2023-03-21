69fdb0456c7e7a4078ecb2bf69a74a918e5a9e53
German <germanburdin1@gmail.com>
2023-03-21 16:34:52 +0100

    Открывается и закрывается (часть 2)

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -17,24 +17,45 @@ function showBigPicture(photo) {
   const commentsList = bigPicture.querySelector('.social__comments');
   commentsList.innerHTML = '';
 
-  photo.comments.forEach((comment) => {
-    const commentElement = document.createElement('li');
-    commentElement.classList.add('social__comment');
+  const commentsLoader = bigPicture.querySelector('.comments-loader');
+  const batchSize = 5;
+  let currentBatchSize = batchSize;
 
-    const avatarElement = document.createElement('img');
-    avatarElement.classList.add('social__picture');
-    avatarElement.src = comment.avatar;
-    avatarElement.alt = comment.name;
-    avatarElement.width = 35;
-    avatarElement.height = 35;
+  function renderComments() {
+    const commentsToShow = photo.comments.slice(0, currentBatchSize);
 
-    const textElement = document.createElement('p');
-    textElement.classList.add('social__text');
-    textElement.textContent = comment.message;
+    commentsToShow.forEach((comment) => {
+      const commentElement = document.createElement('li');
+      commentElement.classList.add('social__comment');
 
-    commentElement.appendChild(avatarElement);
-    commentElement.appendChild(textElement);
-    commentsList.appendChild(commentElement);
+      const avatarElement = document.createElement('img');
+      avatarElement.classList.add('social__picture');
+      avatarElement.src = comment.avatar;
+      avatarElement.alt = comment.name;
+      avatarElement.width = 35;
+      avatarElement.height = 35;
+
+      const textElement = document.createElement('p');
+      textElement.classList.add('social__text');
+      textElement.textContent = comment.message;
+
+      commentElement.appendChild(avatarElement);
+      commentElement.appendChild(textElement);
+      commentsList.appendChild(commentElement);
+    });
+
+    commentsCountElement.textContent = currentBatchSize + ' из ' + photo.comments.length;
+
+    if (currentBatchSize >= photo.comments.length) {
+      commentsLoader.classList.add('hidden');
+    }
+  }
+
+  renderComments();
+
+  commentsLoader.addEventListener('click', () => {
+    currentBatchSize += batchSize;
+    renderComments();
   });
 
   document.body.classList.add('modal-open');
@@ -53,5 +74,10 @@ function showBigPicture(photo) {
     document.removeEventListener('keydown', onEscPress);
   }
 }
+
+const commentCountElement = document.querySelector('.social__comment-count');
+const commentLoaderElement = document.querySelector('.social__comments-loader');
+commentCountElement.classList.remove('hidden');
+commentLoaderElement.classList.remove('hidden');
 
 export {showBigPicture};
